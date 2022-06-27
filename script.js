0309ff1e66280686fedd660d0072d0d04b7157e1
Bruno Sampaio <brunoctsb@gmail.com>
2022-06-27 10:52:13 -0300

    initial js

--- script.js
+++ script.js
@@ -11,11 +11,24 @@ let minutes;
 function countdown() {
 	setTimeout(function () {
 		let seconds = Number(secondsDisplay.textContent);
+		let minutes = Number(minutesDisplay.textContent);
+
+		secondsDisplay.textContent = String(seconds - 1).padStart(2, "0");
+
+		if (minutes <= 0) {
+			buttonPause.classList.add("hide");
+			buttonPlay.classList.remove("hide");
+			buttonStop.classList.add("hide");
+			buttonSet.classList.remove("hide");
+
+			return;
+		}
 
 		if (seconds <= 0) {
-			seconds = 60;
+			seconds = 3;
+			minutesDisplay.textContent = String(minutes - 1).padStart(2, "0");
+			secondsDisplay.textContent = String(seconds - 1).padStart(2, "0");
 		}
-		secondsDisplay.textContent = seconds - 1;
 
 		countdown();
 	}, 1000);
@@ -54,5 +67,5 @@ buttonSoundOff.addEventListener("click", function () {
 
 buttonSet.addEventListener("click", function () {
 	minutes = prompt("Quantos minutos?");
-	minutesDisplay.textContent = minutes;
+	minutesDisplay.textContent = String(minutes).padStart(2, "0");
 });
